b40e42332bdebbaf76963d12338b203804f0f816
awesomebanana2018 <58297401+awesomebanana2018@users.noreply.github.com>
2020-01-12 17:54:24 -0500

    Update PDM.lua
    Does not work unless you add the settings yourself. Getting the file from the release page is recommended.

--- PDM.lua
+++ PDM.lua
@@ -4,20 +4,21 @@ PDM = {}
 Settings = script.Settings
 function PDM:GetStore(Name)
 	local Store = datastore:GetDataStore("DATA")
-	local Data = Store:GetAsync(Name)
+	local Success, Data = xpcall(function() Store:GetAsync(Name)end,function() print("Intial call failed. Retrying once.")Store:GetAsync(Name)end)
 	DataTable[Name] = Data
-	if not Data then DataTable[Name][Settings.AutoSaveKey.Value] = os.time() end
+	if Data == nil then DataTable[Name] = {} DataTable[Name][Settings.AutoSaveKey.Value] = os.time() end
 end
 function PDM:GetAsync(Default,Name,DataName)
 	local Data = DataTable[Name]
-	if Data == nil then local store = PDM:GetStore(Name) return end
+	if Data == nil then local store = PDM:GetStore(Name) end
 		local RData = Data[DataName]
-		if RData == nil then
-			Data[Name] = Default
+		if RData == nil and Default ~= nil then
+			Data[DataName] = Default
+			RData = Data[DataName]
 		end
 		return RData
 	end
-function PDM:Set(StoreName,Name,Value)
+function PDM:SetAsync(StoreName,Name,Value)
 	local Data = DataTable[StoreName]
 	if Name == Settings.AutoSaveKey.Value then print("Request matches AutoSaveKey. Please change data name or AutoSaveKey name.") return end
 	if Data == nil then  local store = PDM:GetStore(StoreName) return end
@@ -31,7 +32,7 @@ function PDM:Update(StoreName)
 	local Data = DataTable[StoreName]
 	if Data == nil then print("Datastore not updated. No update needed") return end
 	local Saved = datastore:GetDataStore("DATA")
-	Saved:SetAsync(StoreName,Data)
+	local S,D = xpcall(function()Saved:SetAsync(StoreName,Data)end,function()print("Intial call failed. Retrying once.") Saved:SetAsync(StoreName,Data)end)
 end
 function PDM:Clear(StoreName)
 	local Data = DataTable[StoreName]
